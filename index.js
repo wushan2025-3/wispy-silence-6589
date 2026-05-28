50f2e6a9b0fdfb168b7153e1a968f7d610aac2c3
wushan2025-3 <wushan2025_3@126.com>
2026-05-28 11:33:56 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 export default {
   async fetch(request, context, env) {
-    const TEST_KEY_PLAIN = env.333;
+    const test1 = env.test1;
+    const test2 = env.test2;
+    const test3 = env.test3;
+    const test4 = env.test4;
+    const test5 = env.test5;
+    const tEst5 = env.tEst5;
+    const tesT6 = env.tesT6;
     
 
-    return new Response(`Get TEST_KEY_PLAIN: ${TEST_KEY_PLAIN} successfully!`);
+    return new Response(`test1 is : ${test1}, test2 is : ${test2}, test3 is : ${test3}, test4 is : ${test4} ,test5 is : ${test5}, tEst5 is : ${tEst5}, tesT6 is : ${tesT6}`);
   },
 };
