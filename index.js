4bec64b24d7dd5c2af6361d35468feed03a01d2d
wushan2025-3 <wushan2025_3@126.com>
2026-05-29 11:38:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 
-export default {
-  async fetch(request, context, env) {
-    const test1 = env.test;
-    const test2 = env.test2;
-    const test3 = env.test3; 
-    const test4 = env.test4;
-    const test5 = env.test5;
-    const tEst5 = env.tEst5;
-    const tesT6 = env.tesT6;
+// export default {
+//   async fetch(request, context, env) {
+//     const test1 = env.test;
+//     const test2 = env.test2;
+//     const test3 = env.test3; 
+//     const test4 = env.test4;
+//     const test5 = env.test5;
+//     const tEst5 = env.tEst5;
+//     const tesT6 = env.tesT6;
     
 
-    return new Response(`test1 is : ${test1}, test2 is : ${test2}, test3 is : ${test3}, test4 is : ${test4} ,test5 is : ${test5}, tEst5 is : ${tEst5}, tesT6 is : ${tesT6}`);
-  },
-};
+//     return new Response(`test1 is : ${test1}, test2 is : ${test2}, test3 is : ${test3}, test4 is : ${test4} ,test5 is : ${test5}, tEst5 is : ${tEst5}, tesT6 is : ${tesT6}`);
+//   },
+// };
  
 
 // export default {
@@ -32,21 +32,21 @@ export default {
 // };
 
 
-// import { env } from "alibaba:workers";
+import { env } from "alibaba:workers";
 
-// const test1 = env.test;
-// const test2 = env.test2;
-// const test3 = env.test3;
-// const test4 = env.test4;
-// const test5 = env.test5;
-// const tEst5 = env.tEst5;
-// const tesT6 = env.tesT6; 
+const test1 = env.test;
+const test2 = env.test2;
+const test3 = env.test3;
+const test4 = env.test4;
+const test5 = env.test5;
+const tEst5 = env.tEst5;
+const tesT6 = env.tesT6; 
 
-// export default {
-//   async fetch(request, context) {
-//     return new Response(`test1 is : ${test1}, test2 is : ${test2}, test3 is : ${test3}, test4 is : ${test4} ,test5 is : ${test5}, tEst5 is : ${tEst5}, tesT6 is : ${tesT6}`);
-//   },
-// };
+export default {
+  async fetch(request, context) {
+    return new Response(`test1 is : ${test1}, test2 is : ${test2}, test3 is : ${test3}, test4 is : ${test4} ,test5 is : ${test5}, tEst5 is : ${tEst5}, tesT6 is : ${tesT6}`);
+  },
+};
 
 
 
